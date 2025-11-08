662477ef0ce516b2148b47cf7512cddd773e0cc4
malte <malte.fillmer@gmail.com>
2025-11-08 23:50:23 +0100

    add playwright

---
+++ tests/e2e/test-1.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from "@playwright/test";
+
+test("test", async ({ page }) => {
+  await page.goto("http://localhost:5173/");
+  await expect(
+    page.getByRole("heading", { name: "Vite + React" })
+  ).toBeVisible();
+});
